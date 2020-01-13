a4b06d63cb4f11ed2ac2c8ae26e86232596d6e73
Sinar Pandora <SinarPandora@outlook.com>
2020-01-13 22:11:01 +0800

    提出 Bot 生成器

--- src/main/scala/o/lartifa/jam/Bootloader.scala
+++ src/main/scala/o/lartifa/jam/Bootloader.scala
@@ -1,12 +1,9 @@
 package o.lartifa.jam
 
-import cc.moecraft.icq.{PicqBotX, PicqConfig}
 import cc.moecraft.logger.format.AnsiColor
-import o.lartifa.jam.common.config.CoolQConfig._
 import o.lartifa.jam.common.config.JamConfig._
 import o.lartifa.jam.common.config.SystemConfig
-import o.lartifa.jam.cool.qq.command.MasterCommands
-import o.lartifa.jam.cool.qq.listener.RuleEngineListener
+import o.lartifa.jam.cool.qq.CoolQQLoader
 import o.lartifa.jam.engine.JamLoader
 import o.lartifa.jam.pool.JamContext
 
@@ -21,14 +18,10 @@ import scala.concurrent.duration.Duration
  */
 object Bootloader {
   def main(args: Array[String]): Unit = {
-    val messageListener = new PicqBotX(new PicqConfig(socketPort).setDebug(SystemConfig.debugMode))
-    messageListener.addAccount(name, postUrl, postPort)
-    messageListener.getEventManager.registerListeners(RuleEngineListener)
-    messageListener.enableCommandManager("！", "!")
-    messageListener.getCommandManager.registerCommands(MasterCommands.commands: _*)
-    JamContext.logger.set(messageListener.getLoggerInstanceManager.getLoggerInstance(name, SystemConfig.debugMode))
+    val client = CoolQQLoader.createCoolQQClient()
+    JamContext.logger.set(client.getLoggerInstanceManager.getLoggerInstance(name, SystemConfig.debugMode))
     Await.result(JamLoader.init(), Duration.Inf)
-    messageListener.startBot()
+    client.startBot()
     JamContext.logger.get().log(s"${AnsiColor.GREEN}${name}已苏醒")
   }
 }
---
+++ src/main/scala/o/lartifa/jam/cool/qq/CoolQQLoader.scala
@@ -0,0 +1,30 @@
+package o.lartifa.jam.cool.qq
+
+import cc.moecraft.icq.{PicqBotX, PicqConfig}
+import o.lartifa.jam.common.config.CoolQConfig.{postPort, postUrl, socketPort}
+import o.lartifa.jam.common.config.JamConfig.name
+import o.lartifa.jam.common.config.SystemConfig
+import o.lartifa.jam.cool.qq.command.MasterCommands
+import o.lartifa.jam.cool.qq.listener.RuleEngineListener
+
+/**
+ * Bot 客户端生成器
+ *
+ * Author: sinar
+ * 2020/1/13 22:03 
+ */
+object CoolQQLoader {
+
+  /**
+   * 创建酷Q客户端
+   * @return 尚未启动的酷Q客户端
+   */
+  def createCoolQQClient(): PicqBotX = {
+    val client = new PicqBotX(new PicqConfig(socketPort).setDebug(SystemConfig.debugMode))
+    client.addAccount(name, postUrl, postPort)
+    client.getEventManager.registerListeners(RuleEngineListener)
+    client.enableCommandManager("！", "!")
+    client.getCommandManager.registerCommands(MasterCommands.commands: _*)
+    client
+  }
+}
