fe8b2e216d31d86359608acc59b45777c21590a3
Sinar Pandora <SinarPandora@outlook.com>
2020-01-16 22:03:46 +0800

    添加注释

--- src/main/scala/o/lartifa/jam/common/config/CoolQConfig.scala
+++ src/main/scala/o/lartifa/jam/common/config/CoolQConfig.scala
@@ -3,13 +3,18 @@ package o.lartifa.jam.common.config
 import com.typesafe.config.{Config, ConfigFactory}
 
 /**
+ * 酷 Q 连接设置
+ *
  * Author: sinar
  * 2019/9/28 15:19
  */
 object CoolQConfig {
   private val config: Config = ConfigFactory.load().getConfig("coolQQ")
 
+  // 酷 Q 服务器地址
   val postUrl: String = config.getString("host")
+  // 消息推送端口
   val postPort: Int = config.getInt("port.post")
+  // 消息接收窗口
   val socketPort: Int = config.getInt("port.socket")
 }
--- src/main/scala/o/lartifa/jam/common/config/SystemConfig.scala
+++ src/main/scala/o/lartifa/jam/common/config/SystemConfig.scala
@@ -5,13 +5,18 @@ import com.typesafe.config.{Config, ConfigFactory}
 import scala.jdk.CollectionConverters._
 
 /**
+ * 系统配置
+ *
  * Author: sinar
  * 2020/1/4 22:46 
  */
 object SystemConfig {
   private val config: Config = ConfigFactory.load().getConfig("system")
 
+  // SSDL 文件目录
   val ssdlPath: String = config.getString("ssdl_path")
+  // SSDL 文件扩展名列表
   val ssdlFileExtension: List[String] = config.getStringList("file_extension").asScala.toList
+  // Debug 模式开启标识
   val debugMode: Boolean = config.getBoolean("debugMode")
 }
