0166df19785e8a8448c0cfdb49ba4d10b829ff4c
Sinar Pandora <SinarPandora@outlook.com>
2020-07-26 20:42:14 +0800

    自动加好友

--- src/main/scala/o/lartifa/jam/common/config/JamConfig.scala
+++ src/main/scala/o/lartifa/jam/common/config/JamConfig.scala
@@ -6,7 +6,7 @@ import com.typesafe.config.{Config, ConfigFactory}
  * 果酱配置
  *
  * Author: sinar
- * 2020/1/2 22:01 
+ * 2020/1/2 22:01
  */
 object JamConfig {
   val config: Config = ConfigFactory.load().getConfig("bot")
@@ -19,4 +19,10 @@ object JamConfig {
 
   // 响应频率
   val responseFrequency: Int = config.getInt("response_frequency")
+
+  // 自动接受好友申请
+  val autoAcceptFriendRequest: Boolean = config.getBoolean("auto_accept_friend_request")
+
+  // 自动接收群申请
+  val autoAcceptGroupRequest: Boolean = config.getBoolean("auto_accept_group_request")
 }
--- src/main/scala/o/lartifa/jam/cool/qq/listener/SystemEventListener.scala
+++ src/main/scala/o/lartifa/jam/cool/qq/listener/SystemEventListener.scala
@@ -1,21 +1,40 @@
 package o.lartifa.jam.cool.qq.listener
 
-import cc.moecraft.icq.event.events.request.EventGroupAddRequest
+import cc.moecraft.icq.event.events.request.{EventFriendRequest, EventGroupAddRequest}
 import cc.moecraft.icq.event.{EventHandler, IcqListener}
+import o.lartifa.jam.common.config.JamConfig
 
 /**
  * 系统事件监听器
  *
  * Author: sinar
- * 2020/1/18 02:59 
+ * 2020/1/18 02:59
  */
 object SystemEventListener extends IcqListener {
 
   /**
-   * 加群时自动刷新系统缓存
+   * 自动加群
    *
    * @param event 加群事件
    */
   @EventHandler
-  def refreshCacheWhenAddToNewGroup(event: EventGroupAddRequest): Unit = event.getBot.getAccountManager.refreshCache()
+  def autoAddGroup(event: EventGroupAddRequest): Unit = {
+    if (JamConfig.autoAcceptGroupRequest) {
+      event.accept()
+    }
+    event.getBot.getAccountManager.refreshCache()
+  }
+
+  /**
+   * 自动加好友
+   *
+   * @param event 加好友事件
+   */
+  @EventHandler
+  def autoAddFriends(event: EventFriendRequest): Unit = {
+    if (JamConfig.autoAcceptFriendRequest) {
+      event.accept()
+    }
+    event.getBot.getAccountManager.refreshCache()
+  }
 }
