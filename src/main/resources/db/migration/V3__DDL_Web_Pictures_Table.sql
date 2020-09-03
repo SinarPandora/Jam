d1625cb29eef66d44ad614ea2683a85c7abb5743
Sinar Pandora <SinarPandora@outlook.com>
2020-09-03 22:15:07 +0800

    更新 flyway

--- src/main/resources/db/migration/V3__DDL_Web_Pictures_Table.sql
+++ src/main/resources/db/migration/V3__DDL_Web_Pictures_Table.sql
@@ -5,14 +5,14 @@ create table web_pictures
         constraint web_pictures_pk
             primary key,
     uid         bigint  default '-1'::integer not null,
-    title       text    default '无标题'::text   not null,
-    author      text    default '未知作者'::text  not null,
-    url         text    default ''::text      not null,
+    title       varchar    default '无标题'::text   not null,
+    author      varchar    default '未知作者'::text  not null,
+    url         varchar    default ''::text      not null,
     is_r18      boolean default false         not null,
     width       integer default '-1'::integer not null,
     height      integer default '-1'::integer not null,
-    tags        text    default ''::text      not null,
-    base64_data text
+    tags        varchar    default ''::text      not null,
+    base64_data varchar
 );
 
 create unique index web_pictures_pid_uindex
