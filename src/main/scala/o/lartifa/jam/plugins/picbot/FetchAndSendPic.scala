fab4ccb68e0dfcd89646e654659b7fcd10f0aa7c
Sinar Pandora <SinarPandora@outlook.com>
2020-09-12 22:14:08 +0800

    JAM-0301 维护 更改图片插件发送图片的错误处理策略

--- src/main/scala/o/lartifa/jam/plugins/picbot/FetchAndSendPic.scala
+++ src/main/scala/o/lartifa/jam/plugins/picbot/FetchAndSendPic.scala
@@ -1,14 +1,17 @@
 package o.lartifa.jam.plugins.picbot
 
 import cc.moecraft.icq.sender.message.components.ComponentImageBase64
+import cc.moecraft.logger.HyLogger
 import o.lartifa.jam.common.util.MasterUtil
 import o.lartifa.jam.database.temporary.schema.Tables
 import o.lartifa.jam.model.CommandExecuteContext
 import o.lartifa.jam.model.commands.Command
+import o.lartifa.jam.plugins.picbot.FetchAndSendPic.logger
+import o.lartifa.jam.pool.JamContext
 
 import scala.async.Async._
 import scala.concurrent.{ExecutionContext, Future}
-import scala.util.Try
+import scala.util.{Failure, Try}
 
 /**
  * 获取并发送图片指令
@@ -56,8 +59,17 @@ case class FetchAndSendPic(amount: Int) extends Command[Unit] {
   def sendPic(record: Tables.WebPicturesRow)(implicit context: CommandExecuteContext): Unit = {
     record.base64Data match {
       case Some(data) => Try(context.eventMessage.respond(new ComponentImageBase64(data).toString))
-        .getOrElse(context.eventMessage.respond("咦，发送失败了，是不是图片太大了。。"))
+        .recover(e => {
+          logger.warning("由于网络原因或图片大小原因发送可能失败。如果图片可以正常显示，请忽略该错误。")
+          logger.error(e)
+          Failure(e)
+        })
       case None => context.eventMessage.respond("咦，这张色图好像被吃掉了？")
     }
   }
+}
+
+
+object FetchAndSendPic {
+  private lazy val logger: HyLogger = JamContext.loggerFactory.get().getLogger(FetchAndSendPic.getClass)
 }
