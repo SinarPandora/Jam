cb326db7a078f6465235eae9878da17c7140faa0
Sinar Pandora <SinarPandora@outlook.com>
2020-10-18 23:10:37 +0800

    JAM-0301 维护 覆盖默认 logger 文件输出模式（使其可以自动输出）

---
+++ jam_self/src/main/scala/cc/moecraft/logger/environments/FileEnv.java
@@ -0,0 +1,81 @@
+package cc.moecraft.logger.environments;
+
+import cc.moecraft.logger.format.AnsiColor;
+import cc.moecraft.utils.FileUtils;
+import lombok.Getter;
+import org.fusesource.jansi.AnsiConsole;
+
+import java.io.*;
+
+import static cc.moecraft.logger.utils.AnsiUtils.removeFormat;
+import static cc.moecraft.utils.TimeUtils.getCurrentTime;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/27 创建!
+ * Created by Hykilpikonna on 2018/05/27!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+@Getter
+public class FileEnv extends LogEnvironment
+{
+    private PrintWriter fileWriter;
+    private File file;
+
+    long lastLogTime = System.currentTimeMillis();
+
+    /**
+     * Construct a file logging environment with a file object.
+     *
+     * @param file File object.
+     */
+    public FileEnv(File file)
+    {
+        this.file = file;
+        if (!file.exists()) FileUtils.createFile(file);
+
+        try
+        {
+            this.fileWriter = new PrintWriter(new BufferedOutputStream(new FileOutputStream(file)), true);
+        }
+        catch (FileNotFoundException e)
+        {
+            throw new RuntimeException("Failed to create file logger:", e);
+        }
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() ->
+        {
+            AnsiConsole.out.println(AnsiColor.GREEN + "检测到进程关闭... 正在保存Log...");
+            fileWriter.flush();
+            fileWriter.close();
+        }));
+    }
+
+    public FileEnv(String filePath, String fileName)
+    {
+        this(getFile(filePath, fileName));
+    }
+
+    private static File getFile(String filePath, String fileName)
+    {
+        if (fileName == null) throw new RuntimeException("Failed to get file:", new NullPointerException());
+        if (filePath == null || filePath.isEmpty()) filePath = "./";
+        if (!(filePath.endsWith("/") || filePath.endsWith("\\"))) filePath += File.separator;
+        return new File(filePath + fileName + "@" + getCurrentTime()
+                .replace(":", "-")
+                .replace(" ", "-") + ".log");
+    }
+
+    @Override
+    public void logRaw(String message)
+    {
+        fileWriter.write(removeFormat(message) + "\n");
+        if (System.currentTimeMillis() - lastLogTime > 5000)
+        {
+            lastLogTime = System.currentTimeMillis();
+            fileWriter.flush();
+        }
+    }
+}
