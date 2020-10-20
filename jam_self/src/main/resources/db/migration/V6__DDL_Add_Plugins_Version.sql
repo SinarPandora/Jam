5fb4bf255445c5e4cf377f0e0d4879782edab2d5
Sinar Pandora <SinarPandora@outlook.com>
2020-10-20 23:00:01 +0800

    JAM-0308 迫害模式插件 Jam 插件自动更新：添加版本号

---
+++ jam_self/src/main/resources/db/migration/V6__DDL_Add_Plugins_Version.sql
@@ -0,0 +1,2 @@
+alter table plugins
+    add version decimal default 0.1 not null;
--- jam_self/src/main/scala/o/lartifa/jam/database/temporary/schema/PluginsTable.scala
+++ jam_self/src/main/scala/o/lartifa/jam/database/temporary/schema/PluginsTable.scala
@@ -15,19 +15,20 @@ trait PluginsTable {
    *  @param author Database column author SqlType(varchar)
    *  @param `package` Database column package SqlType(varchar)
    *  @param installDate Database column install_date SqlType(timestamp)
-   *  @param isEnabled Database column is_enabled SqlType(bool), Default(false) */
-  case class PluginsRow(id: Long, name: String, keywords: String = "", author: String, `package`: String, installDate: java.sql.Timestamp, isEnabled: Boolean = false)
+   *  @param isEnabled Database column is_enabled SqlType(bool), Default(false)
+   *  @param version Database column version SqlType(numeric), Default(0.1) */
+  case class PluginsRow(id: Long, name: String, keywords: String = "", author: String, `package`: String, installDate: java.sql.Timestamp, isEnabled: Boolean = false, version: scala.math.BigDecimal = scala.math.BigDecimal("0.1"))
   /** GetResult implicit for fetching PluginsRow objects using plain SQL queries */
-  implicit def GetResultPluginsRow(implicit e0: GR[Long], e1: GR[String], e2: GR[java.sql.Timestamp], e3: GR[Boolean]): GR[PluginsRow] = GR{
+  implicit def GetResultPluginsRow(implicit e0: GR[Long], e1: GR[String], e2: GR[java.sql.Timestamp], e3: GR[Boolean], e4: GR[scala.math.BigDecimal]): GR[PluginsRow] = GR{
     prs => import prs._
-    PluginsRow.tupled((<<[Long], <<[String], <<[String], <<[String], <<[String], <<[java.sql.Timestamp], <<[Boolean]))
+      PluginsRow.tupled((<<[Long], <<[String], <<[String], <<[String], <<[String], <<[java.sql.Timestamp], <<[Boolean], <<[scala.math.BigDecimal]))
   }
   /** Table description of table plugins. Objects of this class serve as prototypes for rows in queries.
    *  NOTE: The following names collided with Scala keywords and were escaped: package */
   class Plugins(_tableTag: Tag) extends profile.api.Table[PluginsRow](_tableTag, "plugins") {
-    def * = (id, name, keywords, author, `package`, installDate, isEnabled) <> (PluginsRow.tupled, PluginsRow.unapply)
+    def * = (id, name, keywords, author, `package`, installDate, isEnabled, version) <> (PluginsRow.tupled, PluginsRow.unapply)
     /** Maps whole row to an option. Useful for outer joins. */
-    def ? = (Rep.Some(id), Rep.Some(name), Rep.Some(keywords), Rep.Some(author), Rep.Some(`package`), Rep.Some(installDate), Rep.Some(isEnabled)).shaped.<>({ r=>import r._; _1.map(_=> PluginsRow.tupled((_1.get, _2.get, _3.get, _4.get, _5.get, _6.get, _7.get)))}, (_:Any) =>  throw new Exception("Inserting into ? projection not supported."))
+    def ? = (Rep.Some(id), Rep.Some(name), Rep.Some(keywords), Rep.Some(author), Rep.Some(`package`), Rep.Some(installDate), Rep.Some(isEnabled), Rep.Some(version)).shaped.<>({ r=>import r._; _1.map(_=> PluginsRow.tupled((_1.get, _2.get, _3.get, _4.get, _5.get, _6.get, _7.get, _8.get)))}, (_:Any) =>  throw new Exception("Inserting into ? projection not supported."))
 
     /** Database column id SqlType(bigserial), AutoInc, PrimaryKey */
     val id: Rep[Long] = column[Long]("id", O.AutoInc, O.PrimaryKey)
@@ -44,6 +45,8 @@ trait PluginsTable {
     val installDate: Rep[java.sql.Timestamp] = column[java.sql.Timestamp]("install_date")
     /** Database column is_enabled SqlType(bool), Default(false) */
     val isEnabled: Rep[Boolean] = column[Boolean]("is_enabled", O.Default(false))
+    /** Database column version SqlType(numeric), Default(0.1) */
+    val version: Rep[scala.math.BigDecimal] = column[scala.math.BigDecimal]("version", O.Default(scala.math.BigDecimal("0.1")))
 
     /** Uniqueness Index over (`package`) (database name plugins_package_uindex) */
     val index1 = index("plugins_package_uindex", `package`, unique=true)
