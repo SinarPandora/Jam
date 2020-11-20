fba074547a316c314e92aa15c1ebe9b2a1e6ea8b
Sinar Pandora <SinarPandora@outlook.com>
2020-11-21 01:12:41 +0800

    JAM-0309 MemeMaker 获取模板槽

--- plugins/meme_maker/src/main/scala/jam/plugins/meme_maker/engine/MemeAPIResponse.scala
+++ plugins/meme_maker/src/main/scala/jam/plugins/meme_maker/engine/MemeAPIResponse.scala
@@ -27,7 +27,31 @@ object MemeAPIResponse {
     user: String,
     is_new: Boolean,
     is_hot: Boolean,
-  )
+  ) {
+    /**
+     * 获取模板槽（用于插入自定义内容）
+     *
+     * @return 模板信息
+     */
+    def templateSlots: List[String] = {
+      val fillingInfo = read[Map[String, String]](fillings)
+      getTemplateSlots(fillingInfo)
+    }
+
+    /**
+     * 获取模板槽
+     *
+     * @param fillingInfo 模板信息
+     * @param slotId      当前槽编号
+     * @return 模板槽占位符列表
+     */
+    private def getTemplateSlots(fillingInfo: Map[String, String], slotId: Int = 0): List[String] = {
+      fillingInfo.get(s"sentence$slotId") match {
+        case Some(sentence) => sentence +: getTemplateSlots(fillingInfo, slotId + 1)
+        case None => Nil
+      }
+    }
+  }
 
   case class Response[T](ok: Boolean, body: T, msg: String)
 
