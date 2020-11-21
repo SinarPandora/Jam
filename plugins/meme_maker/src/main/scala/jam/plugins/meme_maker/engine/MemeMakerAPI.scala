c53a58b2215fb7e1b2d7e8b2a552dade80f65705
Sinar Pandora <SinarPandora@outlook.com>
2020-11-21 15:01:28 +0800

    JAM-0309 MemeMaker 模板信息缓存

--- plugins/meme_maker/src/main/scala/jam/plugins/meme_maker/engine/MemeMakerAPI.scala
+++ plugins/meme_maker/src/main/scala/jam/plugins/meme_maker/engine/MemeMakerAPI.scala
@@ -1,15 +1,17 @@
 package jam.plugins.meme_maker.engine
 
+import java.time.LocalDateTime
 import java.util.Base64
+import java.util.concurrent.{SynchronousQueue, ThreadPoolExecutor, TimeUnit}
 
 import cc.moecraft.icq.sender.message.components.ComponentImageBase64
 import cc.moecraft.logger.HyLogger
 import jam.plugins.meme_maker.engine.MemeAPIResponse._
-import o.lartifa.jam.common.exception.ExecutionException
 import o.lartifa.jam.pool.JamContext
 import upickle.default._
 
-import scala.util.{Failure, Try}
+import scala.concurrent.{ExecutionContext, ExecutionContextExecutorService, Future}
+import scala.util.{Failure, Success, Try}
 
 /**
  * Meme maker
@@ -39,8 +41,8 @@ object MemeMakerAPI {
     val base64Data = Base64.getEncoder.encodeToString(requests.get(picUrl, readTimeout = 10000).bytes)
     new ComponentImageBase64(base64Data)
   }.recoverWith(err => {
-    logger.error(err)
-    Failure(ExecutionException(s"Gif 生成失败，模板 id 为$id"))
+    logger.error(s"Gif 生成失败，模板 id 为$id", err)
+    Failure(err)
   })
 
   /**
@@ -54,8 +56,8 @@ object MemeMakerAPI {
       requests.post(templateInfoApi, data = Map("id" -> id.toString)).text()
     }.body
   }.recoverWith(err => {
-    logger.error(err)
-    Failure(ExecutionException(s"无法获取模板信息，id 为：$id"))
+    logger.error(s"无法获取模板信息，id 为：$id", err)
+    Failure(err)
   })
 
   /**
@@ -63,10 +65,73 @@ object MemeMakerAPI {
    *
    * @return 模板列表
    */
-  def allTemplates: Try[List[TemplateInfo]] = Try {
-    read[Response[List[TemplateInfo]]](requests.get(templateListApi).text()).body
-  }.recoverWith(err => {
-    logger.error(err)
-    Failure(ExecutionException("获取模板列表失败"))
-  })
+  def allTemplates: List[TemplateInfo] = MemeAPICache.allTemplates
+
+  /**
+   * 初始化
+   */
+  def init(): Unit = {
+    MemeAPICache.refreshTemplates()
+  }
+
+
+  private object MemeAPICache {
+
+    private var cache: List[TemplateInfo] = Nil
+    private var lastUpdate: LocalDateTime = LocalDateTime.now()
+    private var isUpdating: Boolean = false
+    private implicit val singlePool: ExecutionContextExecutorService = ExecutionContext.fromExecutorService(
+      new ThreadPoolExecutor(
+        1,
+        1,
+        0L,
+        TimeUnit.MILLISECONDS,
+        new SynchronousQueue[Runnable]())
+    )
+
+    /**
+     * 获取全部可用模板
+     *
+     * @return 模板列表
+     */
+    def allTemplates: List[TemplateInfo] = {
+      keepTemplateUpdateToDate()
+      cache
+    }
+
+    /**
+     * 保持模板最新
+     */
+    private def keepTemplateUpdateToDate(): Unit = {
+      // 每一小时更新一次模板
+      if ((cache.isEmpty || lastUpdate.isBefore(LocalDateTime.now().minusHours(1))) && !isUpdating)
+        synchronized {
+          // 防止在进入方法的瞬间 flag 变更
+          if (!isUpdating) {
+            isUpdating = true
+            Future {
+              refreshTemplates()
+              isUpdating = false
+            }
+          }
+        }
+    }
+
+    /**
+     * 刷新全部可用模板
+     *
+     * @return 模板列表
+     */
+    def refreshTemplates(): Unit = {
+      val list = Try {
+        read[Response[List[TemplateInfo]]](requests.get(templateListApi).text()).body
+      }.recoverWith(err => {
+        logger.error("获取模板列表失败", err)
+        Success(Nil)
+      }).get
+      if (list.nonEmpty) this.cache = list
+      lastUpdate = LocalDateTime.now()
+    }
+  }
+
 }
