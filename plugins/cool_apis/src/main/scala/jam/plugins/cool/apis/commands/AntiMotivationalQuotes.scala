447b8ff679fc1ccc2131c8c58348c8dbe2c50a58
Sinar Pandora <SinarPandora@outlook.com>
2020-11-27 00:36:33 +0800

    JAM-0310 API集合 & 口袋便签 完成毒鸡汤指令

--- plugins/cool_apis/src/main/scala/jam/plugins/cool/apis/commands/AntiMotivationalQuotes.scala
+++ plugins/cool_apis/src/main/scala/jam/plugins/cool/apis/commands/AntiMotivationalQuotes.scala
@@ -1,8 +1,11 @@
 package jam.plugins.cool.apis.commands
 
+import cc.moecraft.logger.HyLogger
+import o.lartifa.jam.common.exception.ExecutionException
 import o.lartifa.jam.engine.parser.{ParseEngineContext, SSDLCommandParser}
 import o.lartifa.jam.model.CommandExecuteContext
 import o.lartifa.jam.model.commands.Command
+import o.lartifa.jam.pool.JamContext
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -13,6 +16,8 @@ import scala.concurrent.{ExecutionContext, Future}
  * 2020/11/27 00:11
  */
 object AntiMotivationalQuotes extends SSDLCommandParser[AntiMotivationalQuotes.type](SSDLCommandParser.Contains) with Command[String] {
+  private val logger: HyLogger = JamContext.loggerFactory.get().getLogger(AntiMotivationalQuotes.getClass)
+
   /**
    * 解析指令
    *
@@ -30,5 +35,16 @@ object AntiMotivationalQuotes extends SSDLCommandParser[AntiMotivationalQuotes.t
    * @param exec    异步上下文
    * @return 异步返回执行结果
    */
-  override def execute()(implicit context: CommandExecuteContext, exec: ExecutionContext): Future[String] = ???
+  override def execute()(implicit context: CommandExecuteContext, exec: ExecutionContext): Future[String] = Future {
+    val resp = ujson.read(requests.get("https://www.iowen.cn/jitang/api/").text())
+    if (resp("status").num != 1) throw ExecutionException("毒鸡汤 API 调用失败")
+    val amq = resp("data")("content")("content").str
+    context.eventMessage.respond(amq)
+    amq
+  }.recover {
+    case anyErr =>
+      logger.error(anyErr)
+      context.eventMessage.respond("鸡汤被厨师喝掉了...")
+      "毒鸡汤获取失败"
+  }
 }
