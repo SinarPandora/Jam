d7bf79baaa1d79cf9a05a62038fc7669db9b0392
Sinar Pandora <SinarPandora@outlook.com>
2020-12-27 16:49:04 +0800

    JAM-0301 维护 显示原始消息指令

--- jam_self/src/main/scala/o/lartifa/jam/cool/qq/command/MasterCommands.scala
+++ jam_self/src/main/scala/o/lartifa/jam/cool/qq/command/MasterCommands.scala
@@ -1,8 +1,5 @@
 package o.lartifa.jam.cool.qq.command
 
-import java.util
-import java.util.concurrent.Executors
-
 import cc.moecraft.icq.command.interfaces.IcqCommand
 import cc.moecraft.icq.event.events.message.EventMessage
 import cc.moecraft.icq.user.User
@@ -14,6 +11,8 @@ import o.lartifa.jam.model.{ChatInfo, CommandExecuteContext}
 import o.lartifa.jam.plugins.JamPluginLoader
 import o.lartifa.jam.pool.JamContext
 
+import java.util
+import java.util.concurrent.Executors
 import scala.async.Async._
 import scala.concurrent.{ExecutionContext, ExecutionContextExecutor, Future}
 
@@ -32,7 +31,7 @@ object MasterCommands {
     Ping, SessionInfo, Refresh, ReloadSSDL,
     ListVariable, ClearVariableInChat, SetVariable, RemoveVariable,
     ListPlugins, EnablePlugin, DisablePlugin, UninstallPlugin,
-    WakeUpNow, GoASleepNow
+    WakeUpNow, GoASleepNow, ShowRawMessage
   ) ++ JamPluginLoader.loadedComponents.masterCommands
 
   private object Ping extends MasterEverywhereCommand("ping", "在吗") {
@@ -263,7 +262,6 @@ object MasterCommands {
     }
   }
 
-
   private object WakeUpNow extends MasterEverywhereCommand("唤醒", "嗅盐治晕倒") {
     /**
      * 指令操作
@@ -293,6 +291,22 @@ object MasterCommands {
     override def task(event: EventMessage, sender: User, command: String, args: util.ArrayList[String]): Future[String] = {
       GoASleep.goASleep()
       Future.successful(s"${JamConfig.name}已休眠")
+    }
+  }
+
+  private object ShowRawMessage extends MasterEverywhereCommand("原始信息", "raw") {
+    /**
+     * 指令操作
+     *
+     * @param event   消息事件
+     * @param sender  发送者
+     * @param command 指令内容
+     * @param args    参数
+     * @return 输出内容
+     */
+    override def task(event: EventMessage, sender: User, command: String, args: util.ArrayList[String]): Future[String] = Future {
+      event.respond(event.getRawMessage, true)
+      NO_RESPONSE
     }
   }
 
