de616204e78b80cb5b57469a2be9c1868b95ca0a
Sinar Pandora <SinarPandora@outlook.com>
2021-01-23 10:23:18 +0800

    JAM-0307 果酱 GUI 添加注释以及测试脚本

--- jam_wizard/src/main/scala/o/lartifa/jam/controller/InstanceController.scala
+++ jam_wizard/src/main/scala/o/lartifa/jam/controller/InstanceController.scala
@@ -28,6 +28,12 @@ class InstanceController(repo: InstanceRepo) {
   def listAll(): ResponseEntity[Object] =
     ResponseEntity.ok(done(repo.selectAll()))
 
+  /**
+   * 获取指定实例
+   *
+   * @param id 实例 ID
+   * @return 实例数据
+   */
   @GetMapping(Array("/instance/{id}"))
   def get(@PathVariable("id") id: Int): ResponseEntity[Object] =
     repo.selectOne(id) match {
@@ -35,6 +41,12 @@ class InstanceController(repo: InstanceRepo) {
       case None => ResponseEntity.status(HttpStatus.NOT_FOUND).body(error("没有找到指定的实例"))
     }
 
+  /**
+   * 创建实例
+   *
+   * @param record 实例记录
+   * @return 创建结果
+   */
   @PostMapping(Array("/instance"))
   def create(@RequestBody record: Instance): ResponseEntity[Object] = {
     if (!Instance.validateCreate(record))
@@ -48,6 +60,13 @@ class InstanceController(repo: InstanceRepo) {
     }
   }
 
+  /**
+   * 更新实例
+   *
+   * @param id     实例 ID
+   * @param record 实例数据
+   * @return 更新结果
+   */
   @PutMapping(Array("/instance/{id}"))
   def update(@PathVariable("id") id: Int, @RequestBody record: Instance): ResponseEntity[Object] = {
     if (!Instance.validateUpdate(record))
@@ -61,6 +80,12 @@ class InstanceController(repo: InstanceRepo) {
     }
   }
 
+  /**
+   * 删除实例
+   *
+   * @param id 实例 ID
+   * @return 是否成功删除
+   */
   @DeleteMapping(Array("/instance/{id}"))
   def delete(@PathVariable("id") id: Int): ResponseEntity[Object] = {
     repo.delete(id) match {
