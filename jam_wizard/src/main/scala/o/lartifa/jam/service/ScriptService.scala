433792693148d496e6bce9a354161ac63c9d2362
sdong <sinar.shihui.dong@kingland.com>
2021-02-02 10:24:52 +0800

    JAM-0307 果酱 GUI 脚本服务相关方法

--- jam_wizard/src/main/scala/o/lartifa/jam/service/ScriptService.scala
+++ jam_wizard/src/main/scala/o/lartifa/jam/service/ScriptService.scala
@@ -1,6 +1,12 @@
 package o.lartifa.jam.service
 
+import better.files._
+import o.lartifa.jam.common.util.ErrorMsg
+import o.lartifa.jam.model.SXDLScript
+import o.lartifa.jam.utils.FileUtil
 import org.springframework.stereotype.Service
+
+import scala.util.Try
 
 /**
  * 脚本服务
@@ -10,5 +16,25 @@ import org.springframework.stereotype.Service
  */
 @Service
 class ScriptService {
-
+  def saveOrCreate(script: SXDLScript): Try[Unit] = Try {
+    val file = script.dir / script.content / script.ext
+    file.createIfNotExists(createParents = true)
+    file.writeText(script.content)
+  }
+  
+  def getContent(path: String): Either[String, String] = {
+    val script = path.toFile
+    if (script.notExists) Left(ErrorMsg.IO.FILE_NOT_EXIST)
+    else Right(path.toFile.contentAsString)
+  }
+  
+  def delete(path: String, isDir: Boolean = false): Option[String] = {
+    // TODO check if it is root dir of any instance
+    val file = path.toFile
+    if (file.notExists) Some(if (isDir) ErrorMsg.IO.DIR_NOT_EXIST else ErrorMsg.IO.FILE_NOT_EXIST)
+    else {
+      file.delete()
+      None
+    }
+  }
 }
