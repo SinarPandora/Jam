2f2c01e5af4ccca0473ef3f0fd38e0389aa900cc
Sinar Pandora <SinarPandora@outlook.com>
2021-03-25 23:00:21 +0800

    JAM-0301 维护 实装戳一戳和 TTS

--- jam_self/src/main/scala/o/lartifa/jam/engine/ssdl/parser/Patterns.scala
+++ jam_self/src/main/scala/o/lartifa/jam/engine/ssdl/parser/Patterns.scala
@@ -159,9 +159,9 @@ object Patterns {
     // 链接到指定梦境
     val linkToDream: String = "链接到消息中的梦境"
     // 真·戳一戳指令
-    val poke: Regex = """"戳%\{(.+?)}%""".r("qId")
+    val poke: Regex = """戳%\{(.+?)}%""".r("qId")
     // TTS（文本转语音）指令
-    val tts: Regex = """"朗读%\{(.+?)}%""".r("message")
+    val tts: Regex = """朗读%\{(.+?)}%""".r("message")
   }
 
 }
