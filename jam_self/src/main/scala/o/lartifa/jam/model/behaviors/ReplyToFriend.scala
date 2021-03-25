b7f3a2c1da3896d6674ec7f537cb0aabce4a4fda
Sinar Pandora <SinarPandora@outlook.com>
2021-03-25 22:16:24 +0800

    JAM-0301 维护 获取群员 API

--- jam_self/src/main/scala/o/lartifa/jam/model/behaviors/ReplyToFriend.scala
+++ jam_self/src/main/scala/o/lartifa/jam/model/behaviors/ReplyToFriend.scala
@@ -2,12 +2,15 @@ package o.lartifa.jam.model.behaviors
 
 import cc.moecraft.icq.event.events.message.{EventGroupOrDiscussMessage, EventPrivateMessage}
 import cc.moecraft.icq.sender.message.components.ComponentAt
-import cc.moecraft.icq.sender.returndata.ReturnData
+import cc.moecraft.icq.sender.returndata.returnpojo.get.RGroupMemberInfo
 import cc.moecraft.icq.sender.returndata.returnpojo.send.RMessageReturnData
+import cc.moecraft.icq.sender.returndata.{ReturnData, ReturnStatus}
 import o.lartifa.jam.model.CommandExecuteContext
+import o.lartifa.jam.pool.JamContext
 
 import scala.async.Async.{async, await}
 import scala.concurrent.{ExecutionContext, Future}
+import scala.jdk.CollectionConverters._
 import scala.language.implicitConversions
 
 /**
@@ -17,6 +20,37 @@ import scala.language.implicitConversions
  * 2020/9/2 00:56
  */
 trait ReplyToFriend {
+
+  /**
+   * 获取指定群聊的群员列表
+   *
+   * @param groupId 群号
+   * @param exec    异步上下文
+   * @return 群员列表
+   */
+  protected def groupMembers(groupId: Long)(implicit exec: ExecutionContext): Future[Option[List[RGroupMemberInfo]]] = Future {
+    val result = JamContext.httpApi.get()().getGroupMemberList(groupId)
+    if (result.status == ReturnStatus.ok) Some(result.data.asScala.toList)
+    else None
+  }
+
+  /**
+   * 获取当前会话中的群员列表
+   *
+   * @param context 执行上下文
+   * @param exec    异步上下文
+   * @return 群员列表
+   */
+  protected def groupMembers(implicit context: CommandExecuteContext, exec: ExecutionContext): Future[Option[List[RGroupMemberInfo]]] = Future {
+    context.eventMessage match {
+      case message: EventGroupOrDiscussMessage =>
+        val result = JamContext.httpApi.get()().getGroupMemberList(message.getGroup.getId)
+        if (result.status == ReturnStatus.ok) Some(result.data.asScala.toList)
+        else None
+      case _: EventPrivateMessage => None
+    }
+  }
+
   /**
    * 消息中对方的昵称
    * 群：尝试获取群昵称，不存在/失败则使用 QQ 昵称
