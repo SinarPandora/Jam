65b9af8b122167055d9dbc3332057ab83287a0ac
Sinar Pandora <SinarPandora@outlook.com>
2021-06-13 00:07:18 +0900

    JAM-0319 后置指令与彩云回复 联想回复指令

--- jam_self/src/main/scala/o/lartifa/jam/cool/qq/listener/posthandle/AssociatedReply.scala
+++ jam_self/src/main/scala/o/lartifa/jam/cool/qq/listener/posthandle/AssociatedReply.scala
@@ -1,5 +1,11 @@
 package o.lartifa.jam.cool.qq.listener.posthandle
+
 import cc.moecraft.icq.event.events.message.EventMessage
+import cc.moecraft.icq.sender.message.components.ComponentAt
+import cc.moecraft.logger.HyLogger
+import o.lartifa.jam.common.config.JamConfig
+import o.lartifa.jam.plugins.caiyunai.dream.DreamFastClient
+import o.lartifa.jam.pool.JamContext
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -11,6 +17,9 @@ import scala.concurrent.{ExecutionContext, Future}
  * 2021/6/12 21:29
  */
 object AssociatedReply extends PostUnProcessedHandleTask("联想回复") {
+  private val logger: HyLogger = JamContext.loggerFactory.get().getLogger(this.getClass)
+  private val atMe: String = new ComponentAt(JamConfig.qID).toString
+
   /**
    * 执行
    *
@@ -18,5 +27,17 @@ object AssociatedReply extends PostUnProcessedHandleTask("联想回复") {
    * @param exec  异步上下文
    * @return 异步返回执行结果
    */
-  override def execute(event: EventMessage)(implicit exec: ExecutionContext): Future[Unit] = ???
+  override def execute(event: EventMessage)(implicit exec: ExecutionContext): Future[Unit] = {
+    val msg = event.message.replace(" ", "")
+    if (msg.contains(atMe)) {
+      DreamFastClient.reply(content = msg.replace(atMe, "")).map {
+        case Some(response) =>
+          event.respond(response)
+        case None => event.respond("🤔")
+      }.recover(err => {
+        event.respond("😴")
+        logger.error(err)
+      })
+    }
+  }
 }
