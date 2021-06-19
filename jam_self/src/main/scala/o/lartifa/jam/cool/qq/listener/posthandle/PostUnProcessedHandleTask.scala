55c63413e302cded21e128663cc2a2a14df94c32
Sinar Pandora <SinarPandora@outlook.com>
2021-06-19 22:02:12 +0800

    JAM-0301 维护 支持小梦 AI 模型选择

--- jam_self/src/main/scala/o/lartifa/jam/cool/qq/listener/posthandle/PostUnProcessedHandleTask.scala
+++ jam_self/src/main/scala/o/lartifa/jam/cool/qq/listener/posthandle/PostUnProcessedHandleTask.scala
@@ -1,4 +1,5 @@
 package o.lartifa.jam.cool.qq.listener.posthandle
+
 import cc.moecraft.icq.event.events.message.EventMessage
 import o.lartifa.jam.common.util.TriBoolValue
 import o.lartifa.jam.model.CommandExecuteContext
@@ -15,19 +16,19 @@ abstract class PostUnProcessedHandleTask(name: String) extends PostHandleTask(na
   /**
    * 执行
    *
-   * @param event   消息对象
-   * @param context 执行上下文
-   * @param exec    异步上下文
+   * @param event      消息对象
+   * @param contextOpt 执行上下文
+   * @param exec       异步上下文
    * @return 异步返回执行结果
    */
-  override def execute(event: EventMessage, context: Option[CommandExecuteContext])(implicit exec: ExecutionContext): Future[Unit] =
+  override def execute(event: EventMessage, contextOpt: Option[CommandExecuteContext])(implicit exec: ExecutionContext): Future[Unit] =
     this.execute(event)
 
   /**
    * 执行
    *
-   * @param event   消息对象
-   * @param exec    异步上下文
+   * @param event 消息对象
+   * @param exec  异步上下文
    * @return 异步返回执行结果
    */
   def execute(event: EventMessage)(implicit exec: ExecutionContext): Future[Unit]
