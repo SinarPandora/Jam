41f91077a633e65f1e8fda75f303d796e0193292
Sinar Pandora <SinarPandora@outlook.com>
2021-07-07 00:07:25 +0800

    JAM-0301 维护 戳一戳事件

---
+++ jam_self/src/main/scala/o/lartifa/jam/cool/qq/listener/event/PokeEvent.scala
@@ -0,0 +1,44 @@
+package o.lartifa.jam.cool.qq.listener.event
+
+import cc.moecraft.icq.event.events.notice.EventNoticeFriendPoke
+
+import java.sql.Timestamp
+
+/**
+ * 戳一戳事件
+ *
+ * Example: 1211402231 戳 2062406606
+ * {
+ *  "notice_type": "notify", [Support]
+ *  "post_type": "notice",   [Support]
+ *  "self_id": 2062406606,   [Support]
+ *  "sender_id": 1211402231, [Support]
+ *  "sub_type": "poke",      [Support]
+ *  "target_id": 2062406606, [Support]
+ *  "time": 1625586248,      [Partially]
+ *  "user_id": 1211402231    [Support]
+ * }
+ *
+ * Author: sinar
+ * 2021/7/6 22:29
+ */
+case class PokeEvent(event: EventNoticeFriendPoke) extends ExtractableEvent {
+  /**
+   * 事件中的数据
+   *
+   * @return 数据
+   */
+  override def data: Map[String, String] = Map(
+    "目标QQ" -> event.getTargetId.toString,
+    "自己QQ" -> event.getSelfId.toString,
+    "发送者QQ" -> event.getUser.getId.toString,
+    "对方QQ" -> event.getUser.getId.toString,
+    "发送者昵称" -> event.getUser.getInfo.getNickname,
+    "对方昵称" -> event.getUser.getInfo.getNickname,
+    "事件类型" -> event.getPostType,
+    "通知类型" -> event.getNoticeType,
+    "事件子类型" -> event.getSubType,
+    "会话ID" -> event.getUserId.toString,
+    "发送时间" -> new Timestamp(event.getTime * 100).toString
+  )
+}
